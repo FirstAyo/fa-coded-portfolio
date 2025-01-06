c83f7fd3a2116931a8b1f0e65b70ecc9f421c1fa
Festus <techarenaz01@gmail.com>
2025-01-05 19:13:12 -0800

    my first commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+// import Header from "./components/Header";
+import FormInput from "./components/FormInput";
+import Projects from "./components/Projects";
+
+export default function Home() {
+  return (
+    <div className="">
+      {/* <Header /> */}
+      <Projects />
+      <FormInput />
+    </div>
+  );
+}
---
+++ facodedashboard/schemaTypes/index.ts
@@ -0,0 +1,5 @@
+import { authors } from "./authors";
+import { category } from "./category";
+import projects from "./projects";
+
+export const schemaTypes = [projects, category, authors];
